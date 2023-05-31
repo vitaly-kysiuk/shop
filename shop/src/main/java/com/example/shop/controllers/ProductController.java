9a820d47a7c94613d442e5ff92c3a7139811b537
Vitaliy Kisyuk <sulky1230@gmail.com>
2023-05-31 16:55:28 +0300

    New pages

--- shop/src/main/java/com/example/shop/controllers/ProductController.java
+++ shop/src/main/java/com/example/shop/controllers/ProductController.java
@@ -6,7 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProductController {
     @GetMapping("/")
+    public String Main() {
+        return "main";
+    }
+    @GetMapping("/product")
     public String Products() {
         return "products";
+    }
+    @GetMapping("/admin")
+    public String Admin() {
+        return "admin";
     }
 }
---
+++ shop/src/main/java/com/example/shop/models/Product.java
@@ -0,0 +1,5 @@
+package com.example.shop.models;
+
+public class Product {
+
+}
